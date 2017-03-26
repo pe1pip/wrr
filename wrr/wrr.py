23fdb81d1148e47fce680abd00740339e4263dc1
root <remco@pipsworld.nl>
2017-03-26 13:59:33 +0000

    whitespace

--- wrr/wrr.py
+++ wrr/wrr.py
@@ -7,32 +7,32 @@ redis = StrictRedis()
 
 def mkfreq():
 	freq = int()
-    vfo = redis.hget('trx1','vfo')
-    print("vfo: {}".format(vfo))
-    vfo = vfo.decode('ascii')
-    frq = redis.hget('trx1','{}_freq'.format(vfo))
+	vfo = redis.hget('trx1','vfo')
+	print("vfo: {}".format(vfo))
+	vfo = vfo.decode('ascii')
+	frq = redis.hget('trx1','{}_freq'.format(vfo))
 	mode = redis.hget('trx1','{}_mode'.format(vfo))
 	mode = mode.decode('ascii')
-    freq = int(frq.decode('ascii'))
-    tfreq = freq + 1886000000
-    sh = freq % 1000
-    sk = (freq // 1000) % 1000
-    sm = ( freq // 1000000 ) 
-    mh = tfreq % 1000
-    mk = (tfreq // 1000) % 1000
-    mm = ( tfreq // 1000000 )
-    # print("{} {} {}".format(mm, mk, mh))
-    md = '"mm": "{:04d}", "mk": "{:03d}", "mh": "{:03d}"'.format(mm, mk, mh)
-    sd = '"sm": "{:04d}", "sk": "{:03d}", "sh": "{:03d}"'.format(sm, sk, sh)
-    # print("{{ {}, {} }}".format(md, sd))
+	freq = int(frq.decode('ascii'))
+	tfreq = freq + 1886000000
+	sh = freq % 1000
+	sk = (freq // 1000) % 1000
+	sm = ( freq // 1000000 ) 
+	mh = tfreq % 1000
+	mk = (tfreq // 1000) % 1000
+	mm = ( tfreq // 1000000 )
+	# print("{} {} {}".format(mm, mk, mh))
+	md = '"mm": "{:04d}", "mk": "{:03d}", "mh": "{:03d}"'.format(mm, mk, mh)
+	sd = '"sm": "{:04d}", "sk": "{:03d}", "sh": "{:03d}"'.format(sm, sk, sh)
+	# print("{{ {}, {} }}".format(md, sd))
 	fd = '"freq": {{ {}, {} }}'.format(md, sd)
 	md = '"mode": "{}"'.format(mode)
 	vd = '"vfo": "{}"'.format(vfo)
-    return 'data: {{ {},\n{},\n{} }}\n\n'.format(fd, md, vd)
+	return 'data: {{ {},\n{},\n{} }}\n\n'.format(fd, md, vd)
 
 def mkrxled():
-    tx = int(redis.hget('trx1','ptt'))
-    rx = int(redis.hget('trx1','squelsh'))
+	tx = int(redis.hget('trx1','ptt'))
+	rx = int(redis.hget('trx1','squelsh'))
 	led = 'off'
 	if tx:
 		led = 'tx'
@@ -50,7 +50,6 @@ def eventStream():
 			mtype = mtype.decode('ascii')
 			if mtype == 'freq':
 				data = mkfreq()
-				continue
 			if mtype == 'ps':
 				data = mkrxled()
 			if mtype == 'tx':
@@ -59,7 +58,8 @@ def eventStream():
 				continue
 			if mtype == 'dummy':
 				data = 'data: { "dummy": "0" }\n\n'
-            yield data
+			print(data)
+			yield data
 
 	except GeneratorExit:
 		pubsub.unsubscribe('trx1')
